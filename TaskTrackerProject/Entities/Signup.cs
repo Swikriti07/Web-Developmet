476a37b725285b846fe20917f274ce4e58dbdb8f
Swikriti07 <swikritis65@gmail.com>
2026-07-02 08:57:38 +0545

    Initial commit

---
+++ TaskTrackerProject/Entities/Signup.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskTrackerProject.Entities
+
+{
+    public class Signup
+    {
+     public  Guid Id { get; set; }  
+     public string? Name { get; set; }
+     public string? Address { get; set; } 
+     public required string Email { get; set; } 
+     public required string Password { get; set; } 
+     public DateTime DOB { get; set; }
+    }
+}
